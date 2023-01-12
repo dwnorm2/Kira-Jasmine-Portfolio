b8ad03040411f13e4a76841c06499d3ecd37287f
dwnorm2 <dwnorm2@gmail.com>
2023-01-12 16:58:23 -0600

    add js file and optimize contact messages

--- main.js
+++ main.js
@@ -1,23 +1,8 @@
-// //credits to https://codepen.io/annalarson/pen/AegVzq
-// // alert("hi");
-// $(function () {
-//   // $(document).ready shorthand
-//   $(".monster").fadeIn("slow");
-// });
+// alert("hi");
 
 // $(document).ready(function () {
-//   /* Every time the window is scrolled ... */
-//   $(window).scroll(function () {
-//     /* Check the location of each desired element */
-//     $(".hideme").each(function (i) {
-//       var bottom_of_object = $(this).position().top + $(this).outerHeight();
-//       var bottom_of_window = $(window).scrollTop() + $(window).height();
-
-//       /* If the object is completely visible in the window, fade it it */
-//       if (bottom_of_window > bottom_of_object) {
-//         $(this).animate({ opacity: "1" }, 1500);
-//       }
-//     });
+//   $("img").click(function () {
+//     this.requestFullscreen();
 //   });
 // });
 
